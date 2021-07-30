491f82c12eecbfc6692e8a0e8e430c6e0e608612
Surya Kashyap <surya.sk05@outlook.com>
2021-07-30 15:59:30 +0530

    Implement MVVM skeleton with dummy values

--- ConceptPad/Models/Concept.cs
+++ ConceptPad/Models/Concept.cs
@@ -1,23 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConceptPad.Models
 {
-    public class Concept
+    /// <summary>
+    /// A model of a game or app concept
+    /// </summary>
+    public class Concept : INotifyPropertyChanged
     {
         // Attributes
-        public string Name { get; set; }
-        public string Type { get; set; }
-        public string Description { get; set; }
-        public string Framework { get; set; }
-        public DateTime TimeCreated { get; set; }
-        public bool InProduction { get; set; }
+        private string name;
+        private string type;
+        private string description;
+        private DateTime dateCreated;
+        private bool isInProduction;
 
-        public Concept()
+        public string Name
         {
+            get => name;
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
         }
+
+        public string Type
+        {
+            get => type;
+            set
+            {
+                type = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Type)));
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Description)));
+            }
+        }
+
+        public DateTime DateCreated
+        {
+            get => dateCreated;
+            set
+            {
+                dateCreated = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateCreated)));
+            }
+        }
+
+        public bool IsInProduction
+        {
+            get => isInProduction;
+            set
+            {
+                isInProduction = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsInProduction)));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- ConceptPad/Views/MainPage.xaml.cs
+++ ConceptPad/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ConceptPad.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,10 +22,18 @@ namespace ConceptPad.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Concept> concepts;
         public MainPage()
         {
+            concepts = new ObservableCollection<Concept>();
+            concepts.Add(new Concept { Name = "Name", Type ="Game", Description = "Description", DateCreated = DateTime.Now, IsInProduction = false});
+            concepts.Add(new Concept { Name = "Name2", Type ="Game2", Description = "Description", DateCreated = DateTime.Now, IsInProduction = false});
+            concepts.Add(new Concept { Name = "Name3", Type ="Game3", Description = "Description", DateCreated = DateTime.Now, IsInProduction = false});
+            concepts.Add(new Concept { Name = "Name3", Type ="Game3", Description = "Description", DateCreated = DateTime.Now, IsInProduction = false});
+            concepts.Add(new Concept { Name = "Name3", Type ="Game3", Description = "Description", DateCreated = DateTime.Now, IsInProduction = false});
             this.InitializeComponent();
         }
     }
