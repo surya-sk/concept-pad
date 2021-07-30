132cf49d99cffe556a62dd680e576a67cf815a8f
Surya Kashyap <surya.sk05@outlook.com>
2021-07-30 15:06:20 +0530

    Create Concept model

---
+++ ConceptPad/Models/Concept.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptPad.Models
+{
+    public class Concept
+    {
+        // Attributes
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Framework { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public bool InProduction { get; set; }
+
+        public Concept()
+        {
+        }
+    }
+}
