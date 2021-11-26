cb44f0ab515c0a88ed40b45ab5da7da1097358f5
surya.sk <surya.sk05@outlook.com>
2021-11-26 14:48:41 -0400

    Create logger class

---
+++ ConceptPad/Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ConceptPad.Utils
+{
+    public static class Logger
+    {
+        static StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+        static string fileName = "logs.txt";
+
+        public static async Task WriteLogAsync(string log)
+        {
+            string logMessage = $"[{DateTime.Now}] - {log}\n\n";
+            var file = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+            await FileIO.AppendTextAsync(file, logMessage);
+        }
+
+        public static async Task<String> ReadLogsAsync()
+        {
+            var file = await localFolder.GetFileAsync(fileName);
+            string logs = await FileIO.ReadTextAsync(file);
+            return logs;
+        }
+    }
+}
