360cf6a573e437b6d1ade0f32242510bbf3ccb59
surya.sk <surya.sk05@outlook.com>
2021-11-26 14:34:00 -0400

    Show user account info in settings page

--- ConceptPad/Views/SettingsPage.xaml.cs
+++ ConceptPad/Views/SettingsPage.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using ConceptPad.Helpers;
+using System;
+using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.System.Profile;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -14,6 +19,7 @@ namespace ConceptPad.Views
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        string signedIn;
         public string Version
         {
             get
@@ -52,6 +58,34 @@ namespace ConceptPad.Views
             view.BackRequested += View_BackRequested;
         }
 
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            signedIn = ApplicationData.Current.LocalSettings.Values["SignedIn"]?.ToString();
+            if (signedIn == "Yes")
+            {
+                AccountPanel.Visibility = Visibility.Visible;
+                await SetUserPhotoAsync();
+                SignInText.Visibility = Visibility.Collapsed;
+                WarningText.Visibility = Visibility.Visible;
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        private async Task SetUserPhotoAsync()
+        {
+            string userName = ApplicationData.Current.LocalSettings.Values["UserName"]?.ToString();
+            UserNameText.Text = $"Hey there, {userName}! You are all synced.";
+            var cacheFolder = ApplicationData.Current.LocalCacheFolder;
+            var accountPicFile = await cacheFolder.GetFileAsync("profile.png");
+            using (IRandomAccessStream stream = await accountPicFile.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapImage image = new BitmapImage();
+                stream.Seek(0);
+                await image.SetSourceAsync(stream);
+                AccountPic.ProfilePicture = image;
+            }
+        }
+
         private void View_BackRequested(object sender, BackRequestedEventArgs e)
         {
             if(Frame.CanGoBack)
@@ -79,6 +113,11 @@ namespace ConceptPad.Views
         private void TileToggle_Toggled(object sender, RoutedEventArgs e)
         {
             ApplicationData.Current.LocalSettings.Values["LiveTileOn"] = TileToggle.IsOn.ToString();
+        }
+
+        private void SignOutButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
